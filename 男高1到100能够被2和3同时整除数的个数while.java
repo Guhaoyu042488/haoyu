bbb8c3ef327e0e65f451418bb82af949667e0e0a
Guhaoyu042488 <129470260+Guhaoyu042488@users.noreply.github.com>
2023-04-05 17:15:18 +0800

    Add files via upload

---
+++ 男高1到100能够被2和3同时整除数的个数while.java
@@ -0,0 +1,16 @@
+package 谷浩雨的Java;
+
+public class 男高1到100能够被2和3同时整除数的个数while {
+	public static void main(String[] args) {
+		int i=1;
+		int t=0;
+		while(i<=100) {
+		if(i%2==0&&i%3==0) {
+			t++;
+		}
+		i++;
+
+		 }
+       System.out.println(t);
+	}} 
+
